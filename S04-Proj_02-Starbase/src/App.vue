e5b49b5f3563c881af770c58ce286629aea4107e
Vitor <vitorcandrade98@gmail.com>
2017-10-21 15:58:52 -0300

    primeiro comit

---
+++ S04-Proj_02-Starbase/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <Character :id="1"/>
+    </div>
+</template>
+
+<script>
+    import Character from './components/Character.vue'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                title: 'Generate Your Team'
+            }
+        },
+        components: {
+            Character
+        }
+    }
+</script>
---
+++ S04-Proj_02-Starbase/src/components/Character.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+        {{character}}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['id'],
+        data() {
+            return {
+                character: {}
+            }
+        },
+        methods: {
+            fetchCharacter(id) {
+                fetch(`https://swapi.co/api/people/${id}`, {
+                    method: 'GET'
+                })
+                    .then(response => response.json())
+                    .then(json => console.log(json))
+            }
+        },
+        created() {
+            this.fetchCharacter(this.id)
+        }
+    }
+</script>
